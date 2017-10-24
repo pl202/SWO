026578db6bf563c4498e23640b077b81652fdd4d
iagomosqueira <iago@mosqueira.eu>
2017-10-24 21:12:58 +0200

    Only 2 variables left

--- iotcswomse/R/grid.R
+++ iotcswomse/R/grid.R
@@ -6,9 +6,27 @@
 #
 # Distributed under the terms of the European Union Public Licence (EUPL) V.1.1.
 
-# setSWOgrid {{{
-setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
-  base=system.file("sa/2017", package="ioswomse"), name='abt', from=1, write=TRUE) {
+#' Sets a grid of SS3 runs for the IOTC SWO OM
+#'
+#' @name setioswogrid
+#' @examples
+#' base <- '../inst/ext-data/sa/'
+#' name <- 'swo'
+#' from <- 1
+#' write <- TRUE
+#'
+sce <- list(
+  M=c(0.2, 0.4),
+  steepness=c(0.6, 0.75),
+  sigmaR=c(0.2, 0.,6),
+  ess=c(2,20),
+  llq=c(1, 1.01),
+  growmat=c("farley", "wang"),
+  cpue=c("jappt", "jap", "twnpt"))
+
+# setioswogrid {{{
+setioswogrid <- function(sce, cpues, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
+  base=system.file("ext-data/sa", package="ioswomse"), name='swo', from=1, write=TRUE) {
 	
   # EXPAND grid from sce
 	grid <- nameGrid(expand.grid(sce, stringsAsFactors=FALSE), from=from)
@@ -25,7 +43,7 @@ setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
   ctls <- r4ss::SS_readctl_3.24(file=ctlf, use_datlist=T, datlist=dats, verbose=FALSE)
 
   # NAMES in grid
-  pars <- names(grid)[!names(grid) %in% c("number", "id")]
+  pars <- names(sce)
 
   # CREATE dir
   if(dir.exists(dir))
@@ -41,17 +59,14 @@ setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
 
     row <- which(grid$number == i)
  
-    # M
-    if("M" %in% pars) {
-      natM <- c(seq(from=as.numeric(substr(as.character(grid[row,'M']), 1, 2))/10,
-        to=as.numeric(substr(as.character(grid[row,'M']), 3, 4))/10, length=6),
-		  	rep(as.numeric(substr(as.character(grid[row,'M']), 3, 4))/10 , 9))
-      ctl$natM[] <- rep(natM, each=ctl$Ngenders)
-    }
-
-    # sigmaR, #_SRparm3
-    if("sigmaR" %in% pars) {
-      ctl$SRparm["SR_sigmaR", c("INIT", "PRIOR")] <- grid[row, "sigmaR"]
+    # llsel TODO
+    if("llsel" %in% pars) {
+      if(grid[row, 'llsel'] == "DoNorm") {
+        # CHANGE fleet 3 (S) 
+        ctl$size_selex_parms[7:12, "INIT"] <- c(90, -0.50, 5.00, 6.00, -999, 0)
+      } else if(grid[row, 'llsel'] == "Logistic") {
+        ctl$size_selex_parms[7:12, "INIT"] <- c(90, -0.50, 5.00, 6.00, -999, 0)
+      }
     }
 
 		# steepness
@@ -59,18 +74,42 @@ setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
       ctl$SRparm["SR_BH_steep", c("INIT", "PRIOR")] <- grid[row, "steepness"]
     }
 
-    # llsel
-    if("llsel" %in% pars) {
-      if(grid[row, 'llsel'] == "DoNorm") {
-        # CHANGE fleet 3 (S) 
-        ctl$size_selex_parms[7:12, "INIT"] <- c(90, -0.50, 5.00, 6.00, -999, 0)
+    # M
+    if("M" %in% pars) {
+      if(ctl$natM_type == 0) {
+        ctl$MG_parms[1,"INIT"] <- grid[row, "M"]
       }
-
     }
 
-    # cpuecv
-    if("cpuecv" %in% pars) {
-      dat$CPUE$se_log <- grid[row, "cpuecv"]
+    # Growth + maturity
+    if("growmat" %in% pars) {
+      if(grid[row, 'growmat'] == "farley") {
+
+        ctl$MG_parms[2:4,] <- rbind(
+          # Farley Ray L_at_Amin_Fem_GP_1_
+          c(70, 90, 83.22, 83.22, 0, 0.1, -2, 0, 0, 0, 0, 0, 0, 0),
+          # Farley Ray Ray L_at_Amax_Fem_GP_1_
+          c(300, 340, 308.0713, 308.0713, 0, 0.1, -2, 0, 0, 0, 0, 0, 0, 0),
+          # Farley Ray Ray VonBert_K_Fem_GP_1_
+          c(0.05, 0.2, 0.123, 0.123, 0, 0.1, -3, 0, 0, 0, 0, 0, 0, 0))
+
+        # Farley 2016 Otolith
+        ctl$Age_Maturity[] <- c(0.001, 0.006, 0.027, 0.109, 0.354, 0.711, 0.917,
+          0.98, 0.996, 0.999, rep(1, 21))
+
+      } else if(grid[row, 'growmat'] == "wang") {
+        ctl$MG_parms[2:4,] <- rbind(
+          # Wang IO L_at_Amin_Fem_GP_1
+          c(70, 90, 66.2, 66.2, 0, 0.1, -2, 0, 0, 0, 0, 0.5, 0, 0),
+          # Wang IO L_at_Amax_Fem_GP_1
+          c(310, 340, 274.9, 274.9, 0, 0.1, -2, 0, 0, 0, 0, 0.5, 0, 0),
+          # Wang IO VonBert_K_Fem_GP_1
+          c(0.05, 0.26, 0.138, 0.138, 0, 0.1, -3, 0, 0, 0, 0, 0.5, 0, 0))
+
+        # TWN/Hawai'i Maturity 50% age 4
+        ctl$Age_Maturity[] <- c(0, 0, 0, 0, 0.02, 0.1, 0.5, 0.9, 0.98,
+            rep(1, 22))
+      }
     }
 
     # ESS obs
@@ -78,11 +117,48 @@ setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
       dat$lencomp$Nsamp <- grid[row, "ess"]
     }
 	
+    # CPUE scaling schemes TODO
+
+    # CPUEs
+    if("cpue" %in% pars) {
+    # JAP late + PT in SW
+      if(grid[row, "cpue"] == "jappt") {
+        dat$CPUE <- subset(cpues , name != "UJPLL_SW" | year < 2000)[, 1:5]
+        # SET lambdas$value = 0.001 for all but c(13:16,21)
+        ctl$lambdas[, "value"] <- 0.001
+        ctl$lambdas[ctl$lambdas[,"fleet/survey"] %in% c(13:16,21), "value"] <- 1.000
+      }
+
+    # JAP late
+      else if(grid[row, "cpue"] == "jap") {
+        dat$CPUE <- cpues
+        # SET lambdas$value = 0.001 for all but c(13:16)
+        ctl$lambdas[, "value"] <- 0.001
+        ctl$lambdas[ctl$lambdas[,"fleet/survey"] %in% c(13:16), "value"] <- 1.000
+      }
+
+    # TWN late + PT in SW
+      else if(grid[row, "cpue"] == "twnpt") {
+        dat$CPUE <- subset(cpues , name != "UTWLL_SW" | year < 2000)[, 1:5]
+        # SET lambdas$value = 0.001 for all but c(17:21)
+        ctl$lambdas[, "value"] <- 0.001
+        ctl$lambdas[ctl$lambdas[,"fleet/survey"] %in% c(17:21), "value"] <- 1.000
+      }
+
+      dat$N_cpue <- nrow(dat$CPUE)
+    
+    }
+
     # CPUE Q increases
     if("llq" %in% pars) {
       cpue <- data.table(dat$CPUE)
       dat$CPUE <- cpue[, obs:=obs / as.numeric(grid[row, "llq"]) ^ seq(0,
         length(obs) - 1), by=index]
+    }
+
+    # sigmaR, #_SRparm3
+    if("sigmaR" %in% pars) {
+      ctl$SRparm["SR_sigmaR", c("INIT", "PRIOR")] <- grid[row, "sigmaR"]
     }
 
 		# CREATE dir
@@ -104,7 +180,7 @@ setSWOgrid <- function(sce, dir=paste0("grid_", format(Sys.time(), "%Y%m%d")),
 		
     # dat
     r4ss::SS_writedat(dat, outfile=paste0(dirname, "/", name, ".dat"))
-		}
+	}
 
 	invisible(grid)
 } # }}}

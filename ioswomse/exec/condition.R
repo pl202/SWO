8d10b64f52c55996b239300a14d741dd4c3b6712
Iago Mosqueira <iago@mosqueira.eu>
2017-11-17 17:09:47 +0100

    Simpler om, no units

--- ioswomse/exec/condition.R
+++ ioswomse/exec/condition.R
@@ -2,14 +2,14 @@
 # ioalbmse/exec/condition.R
 
 # Copyright European Union, 2016
-# Author: Iago Mosqueira (EC JRC) <iago.mosqueira@jrc.ec.europa.eu>
+# Author: Iago Mosqueira (EC JRC) <iago.mosqueira@ec.europa.eu>
 #
 # Distributed under the terms of the European Union Public Licence (EUPL) V.1.1.
 
 library(ioswomse)
 
 library(doParallel)
-registerDoParallel(6)
+registerDoParallel(225)
 
 # --- SCENARIOS
 
@@ -18,11 +18,17 @@ scenarios <- list(
   M=c(0.2, 0.4),
   # SR steepness
   steepness=c(0.6, 0.75, 0.9),
+  # 
   sigmaR=c(0.2, 0.6),
+  #
   ess=c(2, 20),
+  #
   llq=c(1, 1.01),
+  #
   growmat=c("farley", "wang"),
+  #
   cpue=c("jappt", "jap", "twnpt"),
+  #
   scaling=c("area", "catch", "biomass"))
 
 # --- DATA
@@ -31,7 +37,10 @@ data(cpues)
 
 # -- SETUP grid/SS3 folders
 
+dir <- "grid_20171108"
+
 grid <- setioswogrid(scenarios, cpues=cpues, dir=dir, write=FALSE)
+load(paste0(dir, "/grid.RData"))
 
 # -- RUN SS3 grid
 
@@ -40,32 +49,128 @@ runss3grid(grid, options="", dir=dir)
 # -- LOAD results
 
 # res
-res <- loadres(dir=dir, grid=grid)
+res <- loadres(dir=dir, grid=grid, repfile = "Report.sso.gz",
+  covarfile = "covar.sso.gz")
 
-# refpts: MSY, SB_MSY, F_MSY, SB0
-refpts <- with(res, FLPar(MSY=TotYield_MSY, SBMSY=2 * SSB_MSY, FMSY=Fstd_MSY,
-  SB0=2 * SPB_1950, Ftarget=Fstd_MSY, SBlim=2 * 0.40 * SSB_MSY))
+# omfull
+omf <- loadom(dir=dir, repfile = "Report.sso.gz", covarfile = "covar.sso.gz",
+  compfile="CompReport.sso.gz")
+
+omf <- loadomFLStocks(dir=dir, repfile = "Report.sso.gz", covarfile = "covar.sso.gz",
+  compfile="CompReport.sso.gz")
+omf <- Reduce(combine, om)
+
+omf <- loadomDT(dir=dir, repfile = "Report.sso.gz", covarfile = "covar.sso.gz",
+  compfile="CompReport.sso.gz")
+
+# BUG units in m.spwn & harvest.spwn
+
+range(omf, c("minfbar", "maxfbar")) <- c(2,8)
+name(omf) <- "SWO"
+desc(omf) <- "IOTC SWO SS3 grid 20171117"
+
+
+
+save(omf, res, file="out/omfull.RData", compress="xz")
+
+# fqsfull
+fqs <- metrics(omf)
+
+save(fqs, res, file="out/fqsfull.RData", compress="xz")
 
 # om
-om <- loadom(dir=dir)
-range(om, c("minfbar", "maxfbar")) <- c(2,8)
 
-# sr (residuals)
-resid <- loadrec(paste(dir, grid$id, sep="/"))[['resid']]
+# TRIM if BO > 1.5 Mt
+idb0 <- res$TotBio_Unfished < 1.5e6
 
-predictModel(model=bevholtss3()$model,
-  params=FLPar(s=res$steepness, R0=exp(res$`SR_LN(R0)`), v=res$SPB_1950, iter=dim(res)[1]),
-  FLQuants(residuals=resid[, ac(1975:2012)]))
-  
-osr <- list(model='bevholtss3',
-  params=FLPar(a=res$steepness, b=exp(res$`SR_LN(R0)`), c=res$SPB_1950,
-  iter=dim(res)[1]), formula=rec ~ (4 * a * b * ssb) / (c * (1 - a) + ssb * (5 * a - 1)),
-  residuals=resid[, ac(1975:2012)])
+# TRIM if Convergence level > 0.001
+idcl <- res$Convergence_Level <= 0.001
+
+idx <- idcl & idb0
+
+# SUBSET by idx
+om <- FLCore::iter(omf, idx)
+
+# MERGE areas
+om <- simplify(om, c("area", "unit"))
+
+# DROP iters in slots with var=0
+om <- slim(om)
+# BUG slim if all NAs
+discards(om) <- discards(om)[,,,,,1]
+discards.n(om) <- discards.n(om)[,,,,,1]
+discards.wt(om) <- discards.wt(om)[,,,,,1]
+
+# BUG Should readFLSss3 take care of this?
+landings.wt(om)[1,] <- stock.wt(om)[1,]
+discards.wt(om) <- landings.wt(om)
+discards.n(om) <- 0
+
+# MERGE ages 21-31 in plusgroup
+om <- setPlusGroup(om, 20)
+
+# rpts: MSY, SB_MSY, F_MSY, SB0
+refpts <- with(res[idx,], FLPar(MSY=TotYield_MSY, SBMSY=2 * SSB_MSY, FMSY=Fstd_MSY,
+  SB0=2 * SPB_1950, Ftarget=Fstd_MSY, SBlim=2 * 0.40 * SSB_MSY))
+
+# sr
+resid <- loadquants(dir=dir, object="resid", repfile = "Report.sso.gz",
+  covarfile = "covar.sso.gz", compfile="CompReport.sso.gz")
+
+osr <- predictModel(model="bevholtss3",
+  params=with(res[idx,], FLPar(s=steepness, R0=exp(`SR_LN(R0)`), v=SPB_1950,
+    units=c("", "1000", "t"))),
+  FLQuants(residuals=resid[, ac(1975:2013),,,,idx]))
+
+save(om, refpts, osr, file='out/om.RData', compress='xz')
+
+
+# --- TEST
+library(ioswomse)
+library(FLasher)
+
+data(om)
+
+tt <- fwd(om, sr=list(model=geomean()$model, params=FLPar(a=10000)),
+  control=fwdControl(year=2010:2015, value=6000, quant="catch"))
+
+
+
+
+system.time(tt <- fwd(om, sr=osr, catch=catch(om)[,ac(2013:2015)],
+  residuals=window(osr$residuals, start=1950)))
+
+tt <- fwd(om, sr=osr, catch=catch(om)[,ac(2013:2015)],
+  residuals=window(osr$residuals, start=1950))
+
+tt <- fwd(om, sr=osr,
+  control=fwdControl(year=2010:2012, value=6000, quant="catch"),
+  residuals=window(osr$residuals, start=1950, end=2015))
+
+tt <- fwd(om, sr=list(model=model(osr), params=params(osr)),
+  control=fwdControl(year=2010:2012, value=6000, quant="catch"),
+  residuals=window(osr$residuals, start=1950, end=2015))
+
+tt <- fwd(om, sr=list(model=model(osr), params=params(osr)),
+  control=fwdControl(year=2010:2012, value=6000, quant="catch"),
+  residuals=window(osr$residuals, start=1950, end=2015))
+
+tt <- fwd(om, sr=list(model=model(osr), params=params(osr)),
+  control=fwdControl(year=2010:2015, value=6000, quant="catch"))
+
+
+
+    res <- fwd(ple4, control=control, sr=list(model=geomean()$model, 
+      params=FLPar(a=yearMeans(rec(ple4)[, ac(2006:2008)]))))
+
+# discards
+# landings.wt['0',]
+
 
 # index: cpue, sd, b
 cpue <- foreach(i=seq(length(grid$id))) %dopar% {
   print(i)
-  readFLIBss3(dir=grid$id[i], fleets=c(LLCPUE3=3))
+readFLIBss3(dir=grid$id[i], fleets=c(LLCPUE3=3))
 }
 
 index <- cpue[[1]]@index
@@ -83,57 +188,7 @@ ocpue <- list(index=seasonMeans(index),
   index.res=seasonMeans(index.res)
 ) 
 
-# --- OMF
-# om orp rpts osr res ind
-save(om, rpts, osr, res, ocpue, file='omf.RData', compress='xz')
 
-# --- TRIM down om and res
-
-# CHECK convergence
-id1 <- which(res$Convergence_Level <= 0.001)
-
-# data.frame of K (t) vs. habitat size (km2) for all ALB stocks
-prK <- data.frame(
-	habitat=c(6073, 244, 3752, 7547, 3779, 7426),
-	K=c(474828, NA, 3.576e+5, 3.982e5, 3.5e5, 307830))
-dimnames(prK)[[1]] <- c("IO","MED","NAT","NPA","SAT","SPA")
-
-# lm K ~ 0 + habitat
-mod <- lm(K ~ 0 + habitat, prK)
-
-# 99.9% CI for t/km2 ratio * IO ALB area (Arrizabalaga et al, 2015, doi: 10.1016/j.dsr2.2014.07.001)
-cis <- confint(mod, level = 0.999) * 6073
-
-# idx
-id2 <- which(res$TotBio_Unfished <= cis[2])
-
-# DROP if VB_2014_1 < 2 * C_2014
-vb2014 <- unitSums(quantSums(sel(om)[,'2014',,1] * stock.n(om)[,'2014',,1] * stock.wt(om)[,'2014',,1]))
-id3 <- which(vb2014 > 80000)
-
-# iters TO KEEP
-idx <- Reduce(intersect, list(id1, id2, id3))
-
-# --- OM SUBSET, deGender and deSeason
-# om orp rpts osr res ind
-
-om <- deSS3(FLCore::iter(om, idx))
-  desc(om) <- "IO ALB MSE SS3 grid of 20170117, 655 selected runs"
-orp <- FLBRP(om)
-res <- res[idx, ]
-rpts <- FLPar(MSY=res$TotYield_MSY, SBMSY=2 * res$SSB_MSY, FMSY=res$Fstd_MSY,
-  SB0=2 * res$SPB_1950, Ftarget=res$Fstd_MSY, SBlim=2 * 0.40 * res$SSB_MSY)
-# bevholtss3
-osr <- predictModel(residuals=FLCore::iter(resid,idx)[, ac(1975:2012)],
-  params=FLPar(s=res$steepness, R0=exp(res$`SR_LN(R0)`), v=res$SPB_1950, iter=dim(res)[1]),
-  model=rec ~ (4 * s * R0 * ssb) / (v * (1 - s) + ssb * (5 * s - 1)))
-ocpue <- list(index=ocpue$index,
-  sel.pattern=ocpue$sel.pattern[,,,,,idx],
-  index.q=ocpue$index.q[,,,,,idx],
-  index.res=ocpue$index.res[,,,,,idx]) 
-omp <- FLBRP::fwdWindow(om, orp, end=2040)
-
-save(om, omp, res, rpts, osr, ocpue, file='om.RData', compress='xz')
 
 # --- OMS - For TESTING
 
@@ -158,15 +213,3 @@ ocpue <- list(index=ocpue$index,
 
 save(om, omp, res, rpts, osr, ocpue, file='oms.RData', compress='xz')
 
-# --- OM - SUBSET based on analysis
-
-# GLM
-
-# M 0404 0202 0403
-# h 0.7 0.9
-# ess 20 100
-# cpuecv 0.2 0.5
-
-# PCA
-
-# AI
